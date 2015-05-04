b72afa1562f1a2c19b641633727d6cf1c7cd452a
ShunJen <b00901013@ntu.edu.tw>
2015-05-05 01:08:13 +0800

    poisson

--- blendImg.m
+++ blendImg.m
@@ -27,6 +27,7 @@ img_result_mid = tempImg_1.*weight_1+tempImg_2.*weight_2;
 if 0
     img_result = uint8(img_result_mid);
 else %poisson
+    %weight_blendpatch
     weight_blendpatch = zeros(size(tempImg_1,1),size(tempImg_1,2),3);
     for i=1:(cor_overlap(2)-cor_overlap(1)+1)
         for j=1:(cor_overlap(4)-cor_overlap(3)+1)
@@ -35,24 +36,41 @@ else %poisson
             end
         end
     end
-%     figure();imshow(weight_blendpatch);
-%     figure();imshow(uint8(img_result_mid));
-    [Lh Lv] = imgrad(img_result_mid);
-    [Gh Gv] = imgrad(tempImg_1);
-    Fh = Lh;
-    Fv = Lv;
+    %median filter
+    weight_blendpatch(:,:,1) = medfilt2(weight_blendpatch(:,:,1),[5 5]);
+    weight_blendpatch(:,:,2) = medfilt2(weight_blendpatch(:,:,2),[5 5]);
+    weight_blendpatch(:,:,3) = medfilt2(weight_blendpatch(:,:,3),[5 5]);
+    %weight_blendpatch_shrinked
+    weight_blendpatch_shrinked = zeros(size(tempImg_1,1),size(tempImg_1,2),3);
     for i=1:(cor_overlap(2)-cor_overlap(1)+1)
         for j=1:(cor_overlap(4)-cor_overlap(3)+1)
-            if weight_blendpatch(cor_overlap(1)+i-1+corShift_row,cor_overlap(3)+j-1+corShift_column,:)==ones(1,1,3)
+            if weight_blendpatch((cor_overlap(1)+i-1+corShift_row-2):(cor_overlap(1)+i-1+corShift_row+2),(cor_overlap(3)+j-1+corShift_column-2):(cor_overlap(3)+j-1+corShift_column+2),:)==ones(5,5,3)
+                weight_blendpatch_shrinked(cor_overlap(1)+i-1+corShift_row,cor_overlap(3)+j-1+corShift_column,:) = 1;
+            end
+        end
+    end
+    %img_blendpatch
+    img_blendpatch = tempImg_1.*weight_blendpatch;
+    figure();imshow(img_blendpatch);
+    % gradient
+    globalGradient_H = imfilter(img_result_mid,[ 0,-1, 1 ],'replicate');
+    globalGradient_V = imfilter(img_result_mid,[ 0;-1; 1 ],'replicate');
+    localGradient_H = imfilter(img_blendpatch,[ 0,-1, 1 ],'replicate');
+    localGradient_V = imfilter(img_blendpatch,[ 0;-1; 1 ],'replicate');
+    gradient_H = globalGradient_H;
+    gradient_V = globalGradient_V;
+    for i=1:(cor_overlap(2)-cor_overlap(1)+1)
+        for j=1:(cor_overlap(4)-cor_overlap(3)+1)
+            if weight_blendpatch_shrinked(cor_overlap(1)+i-1+corShift_row,cor_overlap(3)+j-1+corShift_column,:)==ones(1,1,3)
                 img_result_mid(cor_overlap(1)+i-1+corShift_row,cor_overlap(3)+j-1+corShift_column,:) = tempImg_1(cor_overlap(1)+i-1+corShift_row,cor_overlap(3)+j-1+corShift_column,:);
-                Fh(cor_overlap(1)+i-1+corShift_row,cor_overlap(3)+j-1+corShift_column,:) = Gh(cor_overlap(1)+i-1+corShift_row,cor_overlap(3)+j-1+corShift_column,:);
-                Fv(cor_overlap(1)+i-1+corShift_row,cor_overlap(3)+j-1+corShift_column,:) = Gv(cor_overlap(1)+i-1+corShift_row,cor_overlap(3)+j-1+corShift_column,:);
+                gradient_H(cor_overlap(1)+i-1+corShift_row,cor_overlap(3)+j-1+corShift_column,:) = localGradient_H(cor_overlap(1)+i-1+corShift_row,cor_overlap(3)+j-1+corShift_column,:);
+                gradient_V(cor_overlap(1)+i-1+corShift_row,cor_overlap(3)+j-1+corShift_column,:) = localGradient_V(cor_overlap(1)+i-1+corShift_row,cor_overlap(3)+j-1+corShift_column,:);
             end
         end
     end
 %     figure();imshow(uint8(img_result_mid));
-    Y = PoissonJacobi( img_result_mid, Fh, Fv, weight_blendpatch );
-    img_result = uint8(Y);
+    img_blended = poissonBlend( img_result_mid, gradient_H, gradient_V, weight_blendpatch_shrinked );
+    img_result = uint8(img_blended);
 end
 end
 
